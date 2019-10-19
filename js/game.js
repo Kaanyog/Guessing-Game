cea0325b5bf98de76e49757318781f0285e3e452
swifty132 <54196963+swifty132@users.noreply.github.com>
2019-10-19 14:10:54 +1000

    added button
    added button
    @kaanyog can u make it able to be centerd

---
+++ js/game.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+var btn = document.createElement("BUTTON");   // Create a <button> element
+btn.innerHTML = "START";                   // Insert text
+document.body.appendChild(btn);               // Append <button> to <body>
